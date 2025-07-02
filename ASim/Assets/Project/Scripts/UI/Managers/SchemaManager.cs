8d484cd2eb3973aa15f962c4afc8f67e99584482
ArdaYndm <138328104+ardayndm@users.noreply.github.com>
2025-07-02 05:29:07 +0300

    update DynamicGrid.mat, MainScene.unity and SchemaManager.cs

--- ASim/Assets/Project/Scripts/UI/Managers/SchemaManager.cs
+++ ASim/Assets/Project/Scripts/UI/Managers/SchemaManager.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SchemaManager : MonoBehaviour
 {
+    public static List<GameObject> SchemaObjects = new();
+
+    public void AddSchemaObject(GameObject obj) => SchemaObjects.Add(obj);
+
+    /// <summary>
+    /// Tüm şemada bulunan öğeleri siler.
+    /// </summary>
     public void ClearAllSchema()
     {
-        
+        for (int i = SchemaObjects.Count - 1; i >= 0; i--)
+        {
+            if (SchemaObjects[i].transform != null)
+                Destroy(SchemaObjects[i]);
+
+            SchemaObjects.RemoveAt(i);
+        }
     }
 }
